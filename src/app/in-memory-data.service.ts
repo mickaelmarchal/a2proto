bd8d85b55e24d8e33c8778288dca885e29856171
Mickaël Marchal <mickaelmarchal@users.noreply.github.com>
2016-09-30 22:18:06 +0200

    adds new field "age" to heroes

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,16 +1,16 @@
 export class InMemoryDataService {
   createDb() {
     let heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Mr. Nice', age: 54 },
+      { id: 12, name: 'Narco', age: 24 },
+      { id: 13, name: 'Bombasto', age: 22 },
+      { id: 14, name: 'Celeritas', age: 18 },
+      { id: 15, name: 'Magneta', age: 31 },
+      { id: 16, name: 'RubberMan', age: 989 },
+      { id: 17, name: 'Dynama', age: 42 },
+      { id: 18, name: 'Dr IQ', age: 68 },
+      { id: 19, name: 'Magma', age: 12 },
+      { id: 20, name: 'Tornado', age: 34 }
     ];
     return { heroes };
   }
